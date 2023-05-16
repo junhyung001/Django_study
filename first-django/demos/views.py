7f98e26396a00bfccfa5866b40413e2d593ee927
junhyung001 <junhyung8325@gmail.com>
2023-05-16 15:41:27 +0900

    중간 커밋

--- first-django/demos/views.py
+++ first-django/demos/views.py
@@ -3,5 +3,22 @@ from django.http import HttpRequest
 # Create your views here.
 
 def calculator(reqeust):
-    # return HttpRequest('계산기 구현 기능 시작입니다.')
-    return render(reqeust, 'calculator.html')
+    # 1 데이터 확인
+    num1 = reqeust.GET.get('num1')
+    num2 = reqeust.GET.get('num2')
+    opearators = reqeust.GET.get('opearators')
+    
+    # 2 데이터 연산
+    if opearators == '+':
+        result = int(num1) + int(num2)
+    elif opearators == '-':
+        result = int(num1) - int(num2)
+    elif opearators == '*':
+        result = int(num1) * int(num2)
+    elif opearators == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+    
+    # 결과값 보냄
+    return render(reqeust, 'calculator.html', {'result' : result})
